36e188eaf4cb0d4e6b52e08ad0ed023bff2ccf8a
felipeshinozaki56-collab <felipeshinozaki56@gmail.com>
2025-09-21 21:16:37 +0000

    coloquei o nome

--- ArvoreFamiliaCompleta.java
+++ ArvoreFamiliaCompleta.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
+// Aluno: Felipe Hideki Rodrigues Shinozaki 
+// RA: 10438584
 // Classe que representa uma pessoa na árvore genealógica
 class Pessoa {
     String nome;
